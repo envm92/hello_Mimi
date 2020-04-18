ad80365b999b08a5c7c1eb06b90788b4f9573379
Erika Valdes <envm92@gmail.com>
2020-04-17 21:23:15 -0500

    Fisrt version Resume

--- src/components/main/main.component.ts
+++ src/components/main/main.component.ts
@@ -9,6 +9,7 @@ export class MainComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    console.log('🌸%c Hello world! I\'m Eri 👩‍💻, thanks for inspecting my code 🌸', 'background: #222; color: #e91e63');
   }
 
 }
